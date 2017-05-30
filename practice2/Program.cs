53214d8df39fd1903c0a13a436a030d66fc3dcd3
etwilsongit <etwilson.2012@gmail.com>
2017-05-30 11:26:11 -0400

    initial commit

---
+++ practice2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<Enter FIZZ>");
+            int fizz = int.Parse(Console.ReadLine());
+            Console.WriteLine("<Enter BUZZ>");
+            int buzz = int.Parse(Console.ReadLine());
+            Console.WriteLine("<Enter TEST>");
+            int test = int.Parse(Console.ReadLine());
+
+            if (fizz % test == 0 && buzz % test == 0)
+            {
+                Console.WriteLine("fizzbuzz");
+            }
+            else if (fizz % test == 0) 
+            {
+                Console.WriteLine("fizz");
+            }
+            else if (buzz % test == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+            
+
+        }
+    }
+}
